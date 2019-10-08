fbd44f2acac16a982a3620644fd75810b7f89a93
hong <doit0719@163.com>
2019-10-08 21:23:47 +0800

    第四次提交

---
+++ collections/src/com/study/hong/Person.java
@@ -0,0 +1,58 @@
+package com.study.hong;
+
+import java.util.Objects;
+
+public class Person{
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(age, person.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+   /* @Override
+    public int compareTo(Person o) {
+        return 0;//认为元素相同
+       //return this.getAge() - o.getAge();//升序
+        //return o.getAge() - this.getAge();//降序
+    }*/
+}
